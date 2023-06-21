731d2b5a00817239c0f12e75c32f3daff2e954d1
bumsu Choe <hotsem100@gmail.com>
2023-06-21 10:01:51 +0900

    프로젝트 완료

--- public/js/weather.js
+++ public/js/weather.js
@@ -3,8 +3,10 @@ const city = document.querySelector('#weather span:last-child');
 const API_KEY = 'e067c23109efc86fc64a24740ecc1c55';
 
 function onGeoOk(position) {
-  const lat = position.coords.latitude;
-  const lon = position.coords.longitude;
+  // const lat = position.coords.latitude;
+  // const lon = position.coords.longitude;
+  const lat = 35.945152;
+  const lon = 126.682793;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
